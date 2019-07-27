5ad80b367630979a0462b0e2981a903dc3c07d11
fanghang <aifanghang@163.com>
2019-07-27 18:32:28 +0800

    01 Welcome to java

---
+++ 01_Welcome/Welcome.java
@@ -0,0 +1,10 @@
+public class Welcome {
+    /**
+     * 主代码
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+}
