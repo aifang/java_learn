d43e418bc940d745b7f45d64f36d8c4cd0441a22
fanghang <aifanghang@163.com>
2019-07-29 19:15:11 +0800

    02_BallGame moveball

--- 02_MyGame/src/BallGame.java
+++ 02_MyGame/src/BallGame.java
@@ -6,10 +6,40 @@ import javax.swing.*;
  */
 public class BallGame extends JFrame {
 
+    Image ball = Toolkit.getDefaultToolkit().getImage("images/ball.png");
+    Image desk = Toolkit.getDefaultToolkit().getImage("images/desk.png");
+
+    double x = 100;
+    double y = 100;
+
+    public void paint(Graphics g) {
+        System.out.println("重画一次");
+        g.drawImage(desk, 10, 40, null);
+        g.drawImage(ball, (int) x, (int) y, null);
+
+        if (x > 704) {
+            x -= 10;
+
+        }
+        if (x < 4) {
+            x += 10;
+        }
+    }
+
     void launchFrame() {
-        setSize(300, 300);
-        setLocation(400, 400);
+        setSize(710, 390);
+        setLocation(200, 200);
         setVisible(true);
+
+        while (true) {
+            repaint();
+            try {
+                Thread.sleep(40);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void main(String[] args) {
